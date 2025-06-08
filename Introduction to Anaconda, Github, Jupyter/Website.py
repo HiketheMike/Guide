81ac700f127384a94d136600b53f96d76df2cd45
Hikethemike <pandavunguyen@gmail.com>
2025-06-08 16:03:25 +0700

    1

--- Introduction to Anaconda, Github, Jupyter/Website.py
+++ Introduction to Anaconda, Github, Jupyter/Website.py
@@ -33,14 +33,14 @@ if selected_guide == "Anaconda Navigator":
         st.warning("Anaconda logo image not found. Please ensure the image file exists.")
         
     
-    st.write("### **Anaconda Navigator** is your go-to desktop graphical user interface (GUI) for efficient data science and machine learning.")
+    st.write("**Anaconda Navigator** is your go-to desktop graphical user interface (GUI) for efficient data science and machine learning.")
     st.write("""
     - **Streamlined Workflow Management:** Designed to simplify the execution and oversight of your data science and ML projects.
     - **Unified Application Portal:** Provides one-click access to critical tools like **Jupyter Notebook**, **Spyder**, and **VS Code**.
     - **Comprehensive Environment & Package Management:** Ensures a consistent, version-controlled development environment.
     - **Simplified Setup:** A single download eliminates complex installations, getting you productive faster.
     """)
-    
+
     # --- Anaconda Navigator Installation ---
     st.markdown("<a name='anaconda-navigator-installation'></a>", unsafe_allow_html=True)
     st.markdown("<h2 style='color:red;'>Anaconda Navigator Installation</h2>", unsafe_allow_html=True)
