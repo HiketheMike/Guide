a46a10b8c1dbee4e8623029ddf45bed26f535801
Hikethemike <pandavunguyen@gmail.com>
2025-08-11 10:23:31 +0700

    12

--- Introduction to Anaconda, Github, Jupyter/Website.py
+++ Introduction to Anaconda, Github, Jupyter/Website.py
@@ -34,8 +34,7 @@ if selected_guide == "Anaconda Navigator":
         st.markdown("<div style='text-align: center;'> Anaconda Environments tab</div>", unsafe_allow_html=True)
     st.write("") 
     st.write("")
-    
-    st.markdown("<div style='text-align: center;'>Anaconda Home Menu</div>", unsafe_allow_html=True)
+
     st.write("") # Adds a blank line for spacing
     st.markdown("""
     Anaconda Navigator is a **graphical user interface (GUI)** that simplifies the use of Python for environment management and data science. It comes with the Python language when installed, however it also allows for ease of "Virtual Environments" management unlike the individual Python.
